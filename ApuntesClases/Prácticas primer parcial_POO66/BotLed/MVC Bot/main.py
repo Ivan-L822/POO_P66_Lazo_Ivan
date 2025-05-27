3f7c9cb9078e0b2c934a08cb39f9b89af97feabf
Ivan-L822 <ilazov@est.ups.edu.ec>
2025-05-26 21:10:50 -0500

    Update main.py

--- ApuntesClases/Prácticas primer parcial_POO66/BotLed/MVC Bot/main.py
+++ ApuntesClases/Prácticas primer parcial_POO66/BotLed/MVC Bot/main.py
@@ -1,7 +1,7 @@
 from controlador import BotController
 
 if __name__ == "__main__":
-    TOKEN = '7931784865:AAESjqpTO2Yx62fe1xyCGu0wm9ERnYMaAlQ'
+    TOKEN = 'Aquí token'
     LED_PIN = 18
 
     bot_controller = BotController(TOKEN, LED_PIN)
